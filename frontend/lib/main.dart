1096319a78134bb16d58433149e17c69baa0114b
Bruno Sobreira <Brunosfranca15@gmail.com>
2023-05-27 10:56:08 -0300

    main.dart changes

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -15,11 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-<<<<<<< HEAD
       home: const CadastroTerapeuta(title: 'Cadastro de especialista'),
-=======
-      home: const CadastroTerapeuta(),
->>>>>>> 5bd2b76 (My local changes)
     );
   }
 }
