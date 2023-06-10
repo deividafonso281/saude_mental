049775a91472dff976550a554b70e51479b65173
Bruno Sobreira <Brunosfranca15@gmail.com>
2023-06-10 08:56:22 -0300

    Connecting firebase to flutter app

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+FirebaseApp? app; 
+
+Future<void> initializeDefault() async {
+    app = await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+}
+
+void main() async {
+  initializeDefault();
   runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
