407cdd1054fdfa3102411bfaa08471ad313f1149
Bruno Sobreira <Brunosfranca15@gmail.com>
2023-06-10 11:24:28 -0300

    Resolving merge conflict feature/cadastro-especialista-bruno to feature/tela-de-login

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:frontend/screens/auth/Login_screen.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'cadastro_especialista.dart';
 
 FirebaseApp? app;
 
@@ -19,7 +20,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
