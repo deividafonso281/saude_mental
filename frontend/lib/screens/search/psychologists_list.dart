6d38d426b1b0453b664b7484d8f51b7806ff6eb2
biaiamauchi308 <biaiamauchi@gmail.com>
2023-06-22 02:43:17 -0300

    Arrumando o nome dos arquivos

--- frontend/lib/screens/search/psychologists_list.dart
+++ frontend/lib/screens/search/psychologists_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/screens/search/card_psicologo.dart';
+import 'package:frontend/screens/search/psychologist_card.dart';
 
 
 class CardList extends StatefulWidget {
